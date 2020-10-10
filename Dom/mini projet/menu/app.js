03fecf335d5f8582f9c81cc6bec7d34f7c63e1b6
ahmedBou <instinctauto@gmail.com>
2020-10-10 10:10:47 +0100

    make the filter buttons dynamic

--- Dom/mini projet/menu/app.js
+++ Dom/mini projet/menu/app.js
@@ -74,12 +74,32 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
-
-
-
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 window.addEventListener('DOMContentLoaded', function(){
-  let displayMenu = menu.map(function(item){
+  displayMenuItems(menu);
+});
+
+filterBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+      // console.log(menuItem.category);
+      if(menuItem.category === category){
+        return menuItem;
+      }
+    })
+    // console.log(menuCategory);
+    if(category == 'all'){
+      displayMenuItems(menu);
+    }else{
+      displayMenuItems(menuCategory);
+    }
+  })
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
     // console.log(item);
     return `<article class="menu-item">
               <img src=${item.img} class="photo" alt=${item.title}>
@@ -97,4 +117,4 @@ window.addEventListener('DOMContentLoaded', function(){
   displayMenu = displayMenu.join('');
 
   sectionCenter.innerHTML = displayMenu;
-})
+}
